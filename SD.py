88208ebe65dd9ca62e85ae7f93bcab781c871afa
MintgroenePanda <69958666+MintgroenePanda@users.noreply.github.com>
2020-08-21 11:58:33 +0200

    Add files via upload

---
+++ SD.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as pyplot
+import statistics as st
+
+#database toevoegen
+input_file = '/Users/Yasmin/Documents/pwsprogrammeren/treinstoringen.csv'
+data = pd.read_csv('treinstoringen.csv')
+#dataframe df aanmaken
+df = pd.DataFrame(data)
+#column duration_minutes pakken
+df = df[['duration_minutes']]
+#proberen de standard deviaton uit te rekenen en falen
+SD = df.std(axis=None, skipna=None, level=None, ddof=1, numeric_only=None)
+#de resultaten uitprinten zodat ik een leuke error krijg
+print(SD)
