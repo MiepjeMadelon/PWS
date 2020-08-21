ca508af635187a04fed42237180b9c2ef81a85ca
Madelon Bernardy <madelon.bernardy@live.nl>
2020-08-21 10:28:54 +0200

    Commit message

--- databaseOpschonen.py
+++ databaseOpschonen.py
@@ -3,7 +3,7 @@ import pandas as pd;
 
 
 #make the database ready
-trainsData = pd.read_csv("disruptions-2011-2019.csv", names = ['rdt_id', 'ns_lines', 'rdt_lines', 'rdt_lines_id', 'rdt_station_names',
+    trainsData = pd.read_csv("disruptions-2011-2019.csv", names = ['rdt_id', 'ns_lines', 'rdt_lines', 'rdt_lines_id', 'rdt_station_names',
        'rdt_station_codes', 'cause_nl', 'cause_en', 'statistical_cause_nl',
        'statistical_cause_en', 'cause_group', 'start_time', 'end_time',
        'duration_minutes'], header = 0);
@@ -12,14 +12,12 @@ trainsData = pd.read_csv("disruptions-2011-2019.csv", names = ['rdt_id', 'ns_lin
 #cleaning up rows with missing time values
 
     #loop through rows
-for row in trainsData:
-    if 'start_time' == ' ' :
-        data.drop(row, axis = 0 )
+    for row in trainsData:
+        #check if x is empty
+        if 'start_time' == ' ' or 'duration_minutes' == ' ' or 'cause_nl' == ' ' or 'cause_nl' == ' ' or 'statistical_cause_en' == ' ' or 'statistical_cause_nl' == ' ':
+            #if empty, remove entire row
+            data.drop(row, axis = 0 )
 
-
-    #check if x is empty
-
-    #if empty, remove entire row
 
 
 
