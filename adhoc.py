b652f0ba43127f62f5a69e7e2b8d6d02b869d9ba
Madelon Bernardy <madelon.bernardy@live.nl>
2020-11-08 20:03:11 +0100

    new and improved

--- adhoc.py
+++ adhoc.py
@@ -17,7 +17,7 @@ trainsData = pd.read_csv("output_6.csv", names = ['rdt_id', 'ns_lines', 'rdt_lin
        'duration_minutes', 'start_date', 'start_moment', 'start_year',
        'start_month', 'start_day', 'start_hour', 'start_minute', 'start_second'], header = 0);
 data = pd.read_csv('output_6.csv')
-x = 'aanrijding'
+x = 'seinstoring'
 data = data.loc[data['cause_nl'] == x]
 #lets firt make a table with the months and how many minutes it'll take.
 januari = data.loc[data['start_month'] == 1]
@@ -158,8 +158,7 @@ plt.bar(range(len(lentimes)), lentimes)
 plt.xticks(range(len(hours)), hours)
 plt.ylabel("Aantal")
 plt.title("Aantal per uur: " + x)
-plt.show()
-
+#plt.show()
 plt.bar(range(len(lentimes)), minutesperHour)
 plt.xticks(range(len(hours)), hours)
 plt.ylabel("Minutes")
