c348d8e48f7560b315cfc9613d1cc54dff59bb6e
Madelon Bernardy <madelon.bernardy@live.nl>
2021-01-18 12:31:59 +0100

    :(

--- dividingTheData.py
+++ dividingTheData.py
@@ -11,22 +11,39 @@ numTrains = pd.read_csv("webscraperResultV2.csv", names = ['Year', 'Month', 'Day
 allTrainData = allTrainData.loc[allTrainData['start_year'] == 2019]
 numTrains = numTrains.loc[numTrains['Year'] == 2019]
 k = 2019
-perdaynumDevided = [];
+months = [];
+days = [];
+numDataTotal = []; #DATA
+numDataUtrechtCentraal = [];#DATA
+numTrainsUtrechtCentraal = [];#TRAINS
+perdaynumDevidedTotal = [];#BOTH
 for m in range(12):
-
     n = m+1
-    monthlyTrains = numTrains.loc[numTrains['Month'] == n]
-    monthlyData = allTrainData.loc[allTrainData['start_month'] == n]
+    monthlyTrains = numTrains.loc[numTrains['Month'] == n]#TRAINS
+    monthlyData = allTrainData.loc[allTrainData['start_month'] == n]#DATA
     for d in range(31):
         e= d+1
-        dailyTrains = monthlyTrains.loc[monthlyTrains['Day'] == e]
-        dailyData = monthlyData.loc[monthlyData['start_day'] == e]
-        lenDailyData = len(dailyData)
+        dailyData = monthlyData.loc[monthlyData['start_day'] == e]#DATA
+        lenDailyData = len(dailyData)#DATA
         if ((e==29 or e==30 or e==31) and n==2) or (e==31 and (n==4 or n==6 or n==9 or n==11)):
              continue
         else:
-            dailyTrains = monthlyTrains.loc[monthlyTrains['Day'] == e]
-            numDailyTrains = dailyTrains['Trains'].values[0]
-            numcompared = lenDailyData/numDailyTrains
+            dailyTrains = monthlyTrains.loc[monthlyTrains['Day'] == e]#TRAINS
+            numDailyTrains = dailyTrains['Trains'].values[0]#TRAINS
+            numcompared = lenDailyData/numDailyTrains#BOTH
             print('2019' + '-' + str(n) + '-' + str(e))
             print(numcompared)
+            months.append(n)
+            days.append(e)
+            numDataTotal.append(lenDailyData)
+            #This code is not yet correct, it now checks for all rows that day
+            try:
+                if 'Utrecht Centraal' in dailyData['rdt_lines'].values[0]:
+                    print('jup')
+                    numTrainsUtrechtCentraal.append(lenDailyData)
+
+                else:
+                    print('nope')
+            except Exception as e:
+                if True:
+                    print('NOPE')
